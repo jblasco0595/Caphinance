58419f8f06a4018e2c89e91364c9f0bb8894d48a
Jorge Blasco <jorgeblasco0595@gmail.com>
2023-09-05 15:09:26 -0600

    basic user crud with graphql

--- src/main/java/com/caphinance/Controllers/UserController.java
+++ src/main/java/com/caphinance/Controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.caphinance.Resolvers;
+package com.caphinance.Controllers;
 
 import com.caphinance.dtos.UserDTO;
 import com.caphinance.entities.User;
@@ -8,45 +8,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
-
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
-@Component
-public class UserResolver {
-    private static final Logger logger = Logger.getLogger(UserResolver.class.getName());
+@Controller
+public class UserController {
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
 
     @Autowired
     private UserService userService;
 
-    // Query: Obtiene todos los usuarios
-   /* @QueryMapping
+    @QueryMapping(name = "getUsers")
     public List<User> getUsers() {
         return userService.findAll();
     }
-    */
 
-    @QueryMapping(name = "getUsers")
-    public List<User> getUsers() {
-        System.out.println("Hola");
-        logger.info("Executing getUsers method"); // Agregar log de información
-        List<User> users = userService.findAll();
-        logger.info("Found " + users.size() + " users"); // Agregar log con información adicional
-        return users;
-    }
-
-    // Query: Obtiene un usuario por su ID
     @QueryMapping
-    public Optional<User> getUser(@Argument Long id) {
+    public Optional<User> getUser(@Argument(name = "userId") Long id) {
         return userService.findById(id);
     }
 
     // Mutation: Crea un nuevo usuario
     @MutationMapping
-    public User createUser(@Argument(name = "UserInput") UserDTO userDTO) {
+    public User createUser(@Argument(name = "userInput") UserDTO userDTO) {
         User user = new User();
         user.setEmail(userDTO.getEmail());
         return userService.save(user);
@@ -54,7 +41,7 @@ public class UserResolver {
 
     // Mutation: Actualiza un usuario existente
     @MutationMapping
-    public User updateUser(@Argument Long id, @Argument(name = "UserInput") UserDTO userDTO) {
+    public User updateUser(@Argument(name = "userId") Long id, @Argument(name = "userInput") UserDTO userDTO) {
         User user = userService.findById(id).orElseThrow(() -> new InvalidDataException("User not found"));
         user.setEmail(userDTO.getEmail());
         return userService.update(user);
@@ -62,7 +49,7 @@ public class UserResolver {
 
     // Mutation: Elimina un usuario por su ID
     @MutationMapping
-    public Boolean deleteUser(@Argument Long id) {
+    public Boolean deleteUser(@Argument(name = "userId") Long id) {
         userService.deleteById(id);
         return true;
     }
